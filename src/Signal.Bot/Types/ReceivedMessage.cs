ca7ba7c6f32fc4d25ca4929356f13d496b50b53b
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-01-20 13:53:26 +0100

    better Pipeline

--- src/Signal.Bot/Types/ReceivedMessage.cs
+++ src/Signal.Bot/Types/ReceivedMessage.cs
@@ -30,112 +30,112 @@ public class Envelope
 
 public class DataMessage
 {
-    [JsonPropertyName("timestamp")] public long? Timestamp { get; set; } = null;
+    [JsonPropertyName("timestamp")] public long? Timestamp { get; set; }
 
-    [JsonPropertyName("body")] public string? Body { get; set; } = null;
+    [JsonPropertyName("body")] public string? Body { get; set; }
 
-    [JsonPropertyName("attachments")] public List<Attachment>? Attachments { get; set; } = null;
+    [JsonPropertyName("attachments")] public List<Attachment>? Attachments { get; set; }
 
-    [JsonPropertyName("groupV2")] public GroupV2Info? GroupV2 { get; set; } = null;
+    [JsonPropertyName("groupV2")] public GroupV2Info? GroupV2 { get; set; }
 
-    [JsonPropertyName("reaction")] public ReactionData? Reaction { get; set; } = null;
+    [JsonPropertyName("reaction")] public ReactionData? Reaction { get; set; }
 
-    [JsonPropertyName("mentions")] public List<Mention> Mentions { get; set; }
+    [JsonPropertyName("mentions")] public List<Mention>? Mentions { get; set; }
 
-    [JsonPropertyName("quote")] public QuoteData Quote { get; set; }
+    [JsonPropertyName("quote")] public QuoteData? Quote { get; set; }
 
-    [JsonPropertyName("readMessages")] public List<ReadMessage> ReadMessages { get; set; }
+    [JsonPropertyName("readMessages")] public List<ReadMessage>? ReadMessages { get; set; }
 
-    [JsonPropertyName("viewOnce")] public bool ViewOnce { get; set; }
+    [JsonPropertyName("viewOnce")] public bool? ViewOnce { get; set; }
 
-    [JsonPropertyName("previews")] public List<PreviewData> Previews { get; set; }
+    [JsonPropertyName("previews")] public List<PreviewData>? Previews { get; set; }
 }
 
 public class SyncMessage
 {
-    [JsonPropertyName("sentMessage")] public DataMessage SentMessage { get; set; }
+    [JsonPropertyName("sentMessage")] public DataMessage? SentMessage { get; set; }
 }
 
 public class TypingMessage
 {
-    [JsonPropertyName("timestamp")] public long Timestamp { get; set; }
+    [JsonPropertyName("timestamp")] public long? Timestamp { get; set; }
 
-    [JsonPropertyName("action")] public string Action { get; set; }
+    [JsonPropertyName("action")] public string? Action { get; set; }
 }
 
 public class ReceiptMessage
 {
-    [JsonPropertyName("timestamps")] public List<long> Timestamps { get; set; }
+    [JsonPropertyName("timestamps")] public List<long>? Timestamps { get; set; }
 
-    [JsonPropertyName("type")] public string Type { get; set; }
+    [JsonPropertyName("type")] public string? Type { get; set; }
 }
 
 public class Attachment
 {
-    [JsonPropertyName("id")] public string Id { get; set; }
+    [JsonPropertyName("id")] public string? Id { get; set; }
 
-    [JsonPropertyName("filename")] public string Filename { get; set; }
+    [JsonPropertyName("filename")] public string? Filename { get; set; }
 
-    [JsonPropertyName("contentType")] public string ContentType { get; set; }
+    [JsonPropertyName("contentType")] public string? ContentType { get; set; }
 
-    [JsonPropertyName("size")] public long Size { get; set; }
+    [JsonPropertyName("size")] public long? Size { get; set; }
 }
 
 public class GroupV2Info
 {
-    [JsonPropertyName("id")] public string Id { get; set; }
+    [JsonPropertyName("id")] public string? Id { get; set; }
 
-    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("name")] public string? Name { get; set; }
 
-    [JsonPropertyName("revision")] public int Revision { get; set; }
+    [JsonPropertyName("revision")] public int? Revision { get; set; }
 }
 
 public class ReactionData
 {
-    [JsonPropertyName("emoji")] public string Emoji { get; set; }
+    [JsonPropertyName("emoji")] public string? Emoji { get; set; }
 
-    [JsonPropertyName("remove")] public bool Remove { get; set; }
+    [JsonPropertyName("remove")] public bool? Remove { get; set; }
 
-    [JsonPropertyName("targetAuthor")] public string TargetAuthor { get; set; }
+    [JsonPropertyName("targetAuthor")] public string? TargetAuthor { get; set; }
 
     [JsonPropertyName("targetSentTimestamp")]
-    public long TargetSentTimestamp { get; set; }
+    public long? TargetSentTimestamp { get; set; }
 }
 
 public class Mention
 {
-    [JsonPropertyName("start")] public int Start { get; set; }
+    [JsonPropertyName("start")] public int? Start { get; set; }
 
-    [JsonPropertyName("length")] public int Length { get; set; }
+    [JsonPropertyName("length")] public int? Length { get; set; }
 
-    [JsonPropertyName("uuid")] public string Uuid { get; set; }
+    [JsonPropertyName("uuid")] public string? Uuid { get; set; }
 }
 
 public class QuoteData
 {
-    [JsonPropertyName("id")] public long Id { get; set; }
+    [JsonPropertyName("id")] public long? Id { get; set; }
 
-    [JsonPropertyName("author")] public string Author { get; set; }
+    [JsonPropertyName("author")] public string? Author { get; set; }
 
-    [JsonPropertyName("text")] public string Text { get; set; }
+    [JsonPropertyName("text")] public string? Text { get; set; }
 
-    [JsonPropertyName("timestamp")] public long Timestamp { get; set; }
+    [JsonPropertyName("timestamp")] public long? Timestamp { get; set; }
 }
 
 public class ReadMessage
 {
-    [JsonPropertyName("sender")] public string Sender { get; set; }
+    [JsonPropertyName("sender")] public string? Sender { get; set; }
 
-    [JsonPropertyName("timestamp")] public long Timestamp { get; set; }
+    [JsonPropertyName("timestamp")] public long? Timestamp { get; set; }
 }
 
 public class PreviewData
 {
-    [JsonPropertyName("url")] public string Url { get; set; }
+    [JsonPropertyName("url")] public string? Url { get; set; }
 
-    [JsonPropertyName("title")] public string Title { get; set; }
+    [JsonPropertyName("title")] public string? Title { get; set; }
 
-    [JsonPropertyName("description")] public string Description { get; set; }
+    [JsonPropertyName("description")] public string? Description { get; set; }
 
     [JsonPropertyName("image")] public Attachment? Image { get; set; }
 }
