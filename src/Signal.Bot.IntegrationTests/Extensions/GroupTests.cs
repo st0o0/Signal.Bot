dd0f62d647f73a25399d44d2088f5053782bd780
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-02-23 19:59:02 +0100

    Adds documentation for working with attachments

--- src/Signal.Bot.IntegrationTests/Extensions/GroupTests.cs
+++ src/Signal.Bot.IntegrationTests/Extensions/GroupTests.cs
@@ -102,6 +102,7 @@ public class GroupTests : IntegrationTestBase
 
         // Assert
         Assert.Single(MockServer.LogEntries);
+        Assert.Single(result);
     }
 
     [Fact(Timeout = 15000)]
